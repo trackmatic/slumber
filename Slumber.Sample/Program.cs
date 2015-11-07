f654947c2c9a8afa68a040f65750852183c7812c
Ross Jones <rossj@trackmatic.co.za>
2015-11-07 23:13:44 +0200

    fix sample

--- Slumber.Sample/Program.cs
+++ Slumber.Sample/Program.cs
@@ -12,17 +12,6 @@ namespace Slumber.Sample
             {
                 nap.UseJsonSerialization().UseHttp(http => http.ApplicationJson()).UseConsoleLogger();
             });
-
-            client.Execute(new HttpRestRequest<dynamic>("/core/security/authenticate", HttpMethods.Post)
-            {
-                Data = new
-                {
-                    Username = "8310245219089",
-                    Password = "W@kogofU71"
-                }
-            });
-
-            var resposne = client.Execute(new HttpRestRequest<dynamic>("/core/account/get", HttpMethods.Get));
         }
     }
 }
